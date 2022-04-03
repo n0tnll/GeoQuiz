3549af7aa7f5e1471e3d51a6b618c10e0eee4e44
Sergey <frostfreerun@gmail.com>
2022-04-03 22:11:41 +1000

    Creating project

--- app/src/main/java/com/shv/android/geomain/MainActivity.kt
+++ app/src/main/java/com/shv/android/geomain/MainActivity.kt
@@ -2,15 +2,30 @@ package com.shv.android.geomain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var textViewQuestion: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +33,35 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.btnTrue)
         falseButton = findViewById(R.id.btnFalse)
-
+        nextButton = findViewById(R.id.btnNext)
+        textViewQuestion = findViewById(R.id.txtViewQuestion)
 
         trueButton.setOnClickListener { view: View ->
-            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener { view: View ->
-            val toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(false)
         }
+
+        nextButton.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        textViewQuestion.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer)
+            R.string.correct_toast
+        else
+            R.string.incorrect_toast
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/shv/android/geomain/Question.kt
@@ -0,0 +1,5 @@
+package com.shv.android.geomain
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
